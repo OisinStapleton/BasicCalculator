6532b64b9a2789348371fc14f81f96822098afc4
OisinStapleton-STUDE <G00365445@atu.ie>
2025-09-22 10:21:51 +0100

    First commit of BasicCalc

--- src/main/java/ie/atu/Calculator.java
+++ src/main/java/ie/atu/Calculator.java
@@ -3,7 +3,7 @@ package ie.atu;
 import java.util.Scanner;
 
 public class Calculator {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         //Prompt for 1st number
         Scanner scan1 = new Scanner(System.in);
@@ -21,17 +21,26 @@ public class Calculator {
         double sum = firstNum + secondNum;
         System.out.println("The sum is " + sum);
 
-        double operation;
+
+        System.out.println("Please enter an operation (add,subtract,multiply,divide");
+        String operation = scan1.next();
+
+
 
         double result = switch (operation) {
             case "add" -> {
                 yield firstNum + secondNum;
             }
-            case "subtract" -> firstNum - secondNum;
+            case "subtract" ->  firstNum - secondNum;
+
+            case "multiply" -> firstNum * secondNum;
+
+            case "divide" -> firstNum / secondNum;
 
             default -> {
-            System.out.println("Invalid Operation.");
-            yield 0;
+
+                System.out.println("Invalid Operation.");
+                yield 0;
             }
         };
 
